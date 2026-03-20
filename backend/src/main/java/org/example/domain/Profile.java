92fc035f876276b75b712edf4449b9e8b7043e10
tkushmyruk-CLGX <tkushmyruk@corelogic.com>
2026-03-20 21:38:38 +0200

    MSN-1 add profile page

---
+++ backend/src/main/java/org/example/domain/Profile.java
@@ -0,0 +1,12 @@
+package org.example.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class Profile {
+    public String profileName;
+}
